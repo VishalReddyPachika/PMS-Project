6ab9a2f1dc6d3de6d615d3d304086958b8155ff0
VishalReddyPachika <pachikavishalreddy@gmail.com>
2022-11-01 17:24:35 -0400

    Created Registration Page Sprint 1

---
+++ PassportManagementSystem/Controllers/PassportManagementSystemController.cs
@@ -0,0 +1,46 @@
+﻿using PassportManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassportManagementSystem.Controllers
+{
+    [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]//Prevents Page from going back when back button is pressed
+    public class PassportManagementSystemController : Controller
+    {
+        //Displays Home View
+        public ActionResult Home()
+        {
+            return View();
+        }
+        //Displays Register View
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //When user submits the form in register page it validates
+        //If validation is successfull then it goes to DBOperations Class and
+        //fetches the data and store in ViewBag.data(used in .cshtml to display) and empty the fiels in the form
+        //Else it returns to the same view with validation messages
+        [HttpPost]
+        public ActionResult Register(UserRegistration R)
+        {
+            R.ApplyType = "Passport User";
+            ModelState.Remove("UserID");
+            ModelState.Remove("ApplyType");
+            ModelState.Remove("ConfirmPassword");
+            if (ModelState.IsValid)
+            {
+                UserRegistration details = DBOperations.Registration(R);
+                ViewBag.data = details;
+                ModelState.Clear();
+                return View();
+            }
+            else
+                return View();
+        }
+    }
+}
---
+++ PassportManagementSystem/Models/DBOperations.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PassportManagementSystem.Models
+{
+    public class DBOperations
+    {
+        static PassportManagementSystemEntities P = new PassportManagementSystemEntities();
+
+        //This method generates UserID,Password and CitizenType based on validations and conditions mentioned in the SRD and insert in database
+        public static UserRegistration Registration(UserRegistration R)
+        {
+            try
+            {
+                string citizentype = string.Empty;
+                string userid = string.Empty;
+
+                if (R.ApplyType == "Passport User")
+                {
+                    int passid = (from c in P.UserRegistrations
+                                  where c.ApplyType == R.ApplyType
+                                  select c).Count() + 1;
+                    userid = R.ApplyType.Substring(0, 4).ToUpper() + "-" + string.Format("{0:0000}", passid);
+                }
+                else if (R.ApplyType == "Visa")
+                {
+                    int visaid = (from c in P.UserRegistrations
+                                  where c.ApplyType == R.ApplyType
+                                  select c).Count() + 1;
+                    userid = R.ApplyType.Substring(0, 4).ToUpper() + "-" + string.Format("{0:0000}", visaid);
+                }
+
+                //CitizenType
+                int age = (int)(DateTime.Today.Subtract(R.DateOfBirth).TotalDays / 365);
+                if (age >= 0 && age < 1)
+                    citizentype = "Infant";
+                else if (age >= 1 && age < 10)
+                    citizentype = "Children";
+                else if (age >= 10 && age < 20)
+                    citizentype = "Teen";
+                else if (age >= 20 && age < 50)
+                    citizentype = "Adult";
+                else if (age >= 50)
+                    citizentype = "Senior Citizen";
+
+                //Inserting into Database
+                R.UserID = userid;
+                R.CitizenType = citizentype;
+                P.UserRegistrations.Add(R);
+                P.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            return R;
+        }
+        //This method checks seperately for 'Passport' whether the EmailId is already registered 
+        public static bool EmailValidation(UserRegistration R)
+        {
+            try
+            {
+                List<string> emailIDs = (from u in P.UserRegistrations
+                                         where u.ApplyType == R.ApplyType
+                                         select u.EmailAddress).ToList();
+                if (emailIDs.Contains(R.EmailAddress))
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception) { }
+            return false;
+        }
+    }
+}
